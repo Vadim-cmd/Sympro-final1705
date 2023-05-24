4773b516b68847755ef53f2e0e50aa4c7b29de72
Vadim <vadimyatsunyk@gmail.com>
2023-05-24 15:05:45 +0200

    1505

--- script.js
+++ script.js
@@ -19,3 +19,28 @@ function menuOnClick() {
   document.getElementById("menu").classList.toggle("change");
   document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+
+// ============slider
+$(document).ready(function(){
+  $('.customer-logos').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+          breakpoint: 768,
+          settings: {
+              slidesToShow: 4
+          }
+      }, {
+          breakpoint: 520,
+          settings: {
+              slidesToShow: 3
+          }
+      }]
+  });
+});
